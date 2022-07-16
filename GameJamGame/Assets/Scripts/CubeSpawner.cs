50deba62b2026e51d341e5a883d7cf7f01dc1f8b
PetarBabic <43879864+PetarBabic@users.noreply.github.com>
2022-07-16 12:15:14 +0200

    Renamed cube spawner script

--- GameJamGame/Assets/Scripts/CubeSpawner.cs
+++ GameJamGame/Assets/Scripts/CubeSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeTest : MonoBehaviour
+public class CubeSpawner : MonoBehaviour
 {
     public GameObject cubePrefab;
 
